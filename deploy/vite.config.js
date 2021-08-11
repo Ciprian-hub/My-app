5c3e450f61abffe7969f7671cfd48b31bd07cbc0
unknown <cipssul@gmail.com>
2021-08-11 18:04:57 +0300

    sss

--- deploy/vite.config.js
+++ deploy/vite.config.js
@@ -3,5 +3,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://ciprian-hub.github.io/deploy/',
   plugins: [reactRefresh()]
 })
