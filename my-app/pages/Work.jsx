6a4b5ac1d8bd33a0df4e31e882c1e02de23ebc0e
unknown <cipssul@gmail.com>
2021-08-24 18:25:46 +0300

    add-projects-test

--- my-app/pages/Work.jsx
+++ my-app/pages/Work.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import Wrapper from '../components/Wrapper'
+import { Link } from 'react-router-dom'
 import './Work.css'
 
 
 function Work() {
     return (
         <div className="page-box">
-            <Wrapper/>
+            <Wrapper />
             <div className="content-work">
                 <section className='java-projects'>
                     <h2>Javscrip</h2>
                     <div className="java-links">
-                        <div></div>
+                        <a href={'https://github.com/Ciprian-hub/My-Projects/tree/dad-jokes/DadJokes'}>
+                            <div className="dad-jokes">Dad</div>
+                            Dad Jokes
+                        </a>
                         <div></div>
                         <div></div>
                     </div>
                 </section>
                 <section className='vue-projects'>
-                <h2>Vue</h2>
+                    <h2>Vue</h2>
                     <div className="vue-links">
                         <div></div>
                         <div></div>
@@ -25,7 +29,7 @@ function Work() {
                     </div>
                 </section>
                 <section className='react-projects'>
-                <h2>React</h2>
+                    <h2>React</h2>
                     <div className="react-links">
                         <div></div>
                         <div></div>
