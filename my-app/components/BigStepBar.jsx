c97437c62cc537ccc489a1475734e0e439fe0827
unknown <cipssul@gmail.com>
2021-08-30 11:35:42 +0300

    add-svg-components

---
+++ my-app/components/BigStepBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function BigStepBar() {
+    return (
+        <div>
+            <svg id='bigSteper' width="18" height="313" viewBox="0 0 18 313" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 17.9999L8.99998 109.636" stroke="white" />
+                <path d="M9 237.273L9 312.545" stroke="white" />
+                <circle cx="9" cy="9" r="9" fill="#FF9900" fill-opacity="0.87" />
+                <path d="M18 118.636C18 123.607 13.9706 127.636 9 127.636C4.02944 127.636 0 123.607 0 118.636C0 113.666 4.02944 109.636 9 109.636C13.9706 109.636 18 113.666 18 118.636Z" fill="#FF9900" fill-opacity="0.87" />
+                <circle cx="9" cy="228.273" r="8.5" stroke="#FF9900" stroke-opacity="0.87" />
+                <path d="M8.9992 127.636L8.99855 219.273" stroke="white" />
+            </svg>
+
+        </div>
+    )
+}
+
+export default BigStepBar
